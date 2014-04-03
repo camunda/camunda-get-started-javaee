a9d2c22c0a3aa284c3d16ca1e804e96d4c987765
Sebastian Menski <sebastian.menski@googlemail.com>
2014-04-03 11:33:24 +0200

    Step 5: add entity bean and EJB
    In this step we add an entity bean for the order entity. Add an
    EJB to persist the order and call it from the persist order service
    task.
    
    See:
    http://camunda.org/get-started/java-ee.html#tutorial-add-persist-service-task

---
+++ src/main/java/org/camunda/bpm/getstarted/pizza/OrderBusinessLogic.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. Camunda licenses this file to you under the Apache License,
+ * Version 2.0; you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.camunda.bpm.getstarted.pizza;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+
+import javax.ejb.Stateless;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.Map;
+
+@Stateless
+@Named
+public class OrderBusinessLogic {
+
+  // Inject the entity manager
+  @PersistenceContext
+  private EntityManager entityManager;
+
+  public void persistOrder(DelegateExecution delegateExecution) {
+    // Create new order instance
+    OrderEntity orderEntity = new OrderEntity();
+
+    // Get all process variables
+    Map<String, Object> variables = delegateExecution.getVariables();
+
+    // Set order attributes
+    orderEntity.setCustomer((String) variables.get("customer"));
+    orderEntity.setAddress((String) variables.get("address"));
+    orderEntity.setPizza((String) variables.get("pizza"));
+
+    // Persist order instance and flush. After the flush the
+    // id of the order instance is set.
+    entityManager.persist(orderEntity);
+    entityManager.flush();
+
+    // Remove no longer needed process variables
+    delegateExecution.removeVariables(variables.keySet());
+
+    // Add newly created order id as process variable
+    delegateExecution.setVariable("orderId", orderEntity.getId());
+  }
+
+}
